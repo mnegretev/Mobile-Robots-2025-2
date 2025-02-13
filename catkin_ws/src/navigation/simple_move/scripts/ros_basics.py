952b4e4b59d1c701abef61126e9bb60e66c33c0f
StephanieMonjaraz <jesssicagarcia9@gmail.com>
2025-02-13 14:09:57 -0600

    Modifique el codigo en el archivo solicitado segun las actividades de la tarea 2

--- catkin_ws/src/navigation/simple_move/scripts/ros_basics.py
+++ catkin_ws/src/navigation/simple_move/scripts/ros_basics.py
@@ -13,15 +13,16 @@ import rospy
 from sensor_msgs.msg   import LaserScan
 from geometry_msgs.msg import Twist
 
-NAME = "WRITE_HERE_YOUR_FULL_NAME"
+NAME = "Jessica Stephanie Garcia Monjaraz"
 
 def callback_scan(msg):
     global obstacle_detected
     #
     # TODO:
-    # Do something to detect if there is an obstacle in front of the robot.
-    # Set the 'obstacle_detected' variable with True or False, accordingly.
-    #
+    # Se calcula el índice central del arreglo de las mediciones de angulos que toma el LiDAR, la mitad sería el frente del robot. 
+    n = int((msg.angle_max - msg.angle_min) / msg.angle_increment / 2)
+    # Se detecta un obstáculo basado en la distancia umbral (1m), dará un booleano, es decir, si hay un objeto o no lo hay. 
+    obstacle_detected = msg.ranges[n] < 1.0
     
     return
 
@@ -37,11 +38,13 @@ def main():
     while not rospy.is_shutdown():
         #
         # TODO:
-        # Declare a Twist message and assign the appropiate speeds:
-        # Move forward if there is no obstacle in front of the robot, and stop otherwise.
-        # Use the 'obstacle_detected' variable to check if there is an obstacle. 
-        # Publish the Twist message using the already declared publisher 'pub_cmd_vel'.
-        
+        # Se crea el mensaje Twist (msg)
+        msg_cmd_vel = Twist()
+        # Define la velocidad, si hay un obstaculo, no se mueve (0), si no hay obstáculo, avanza (0.3)
+        msg_cmd_vel.linear.x = 0 if obstacle_detected else 0.3
+        # Use the 'obstacle_detected' variable to check if there is an obstacle, (lo que se hizo en la linea 43). 
+        # Publica el mensaje de velocidad. (msg_cmd_vel)
+        pub_cmd_vel.publish(msg_cmd_vel)
         
         loop.sleep()
 
