ec8f22f61b4b50b7a19a3b1a967f98412ad15820
xavisuaste <kevin.suastegui@ingenieria.unam.edu>
2025-02-18 01:13:46 -0600

    Update ros_basics.py

--- catkin_ws/src/navigation/simple_move/scripts/ros_basics.py
+++ catkin_ws/src/navigation/simple_move/scripts/ros_basics.py
@@ -12,7 +12,7 @@ import rospy
 from sensor_msgs.msg   import LaserScan
 from geometry_msgs.msg import Twist
 
-NAME = "WRITE_HERE_YOUR_FULL_NAME"
+NAME = "XAVIER SUÁSTEGUI RODRÍGUEZ"
 
 def callback_scan(msg):
     global obstacle_detected
@@ -22,7 +22,10 @@ def callback_scan(msg):
     # Set the 'obstacle_detected' variable with True or False, accordingly.
     #
     
+    n = int((msg.angle_max - msg.angle_min)/msg,angle_increment/2)
+    obstacle_detected = msg.ranges[n] < 1.0
     return
+
 
 def main():
     print("ROS BASICS - " + NAME)
@@ -34,14 +37,15 @@ def main():
     global obstacle_detected
     obstacle_detected = False
     while not rospy.is_shutdown():
-        #
         # TODO:
         # Declare a Twist message and assign the appropiate speeds:
         # Move forward if there is no obstacle in front of the robot, and stop otherwise.
         # Use the 'obstacle_detected' variable to check if there is an obstacle. 
         # Publish the Twist message using the already declared publisher 'pub_cmd_vel'.
-        
-        
+        msg_cmd_vel = Twist()
+        msg_cmd_vel.linear.x = 0 if obstacle_detected else 0.3
+        pub_cmd_vel.publish(msg_cmd_vel)
+        msg
         loop.sleep()
 
 
