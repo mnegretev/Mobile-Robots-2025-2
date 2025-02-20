7bb0302c3bfcdab72153014f95f6dac58de885cb
DeltaCharly <carlos.frausto@comunidad.unam.mx>
2025-02-20 11:39:01 -0600

    Tarea 4

--- catkin_ws/src/navigation/path_planner/scripts/rrt.py
+++ catkin_ws/src/navigation/path_planner/scripts/rrt.py
@@ -19,7 +19,7 @@ from nav_msgs.msg import Path
 from nav_msgs.srv import *
 from collections import deque
 
-NAME = "FULL NAME"
+NAME = "Frausto Martinez Juan Carlos"
 
 class Node:
     def __init__(self, x, y, parent=None):
@@ -79,14 +79,16 @@ def rrt(start_x, start_y, goal_x, goal_y, grid_map, epsilon, max_attempts):
     tree = Node(start_x, start_y)
     goal_node = Node(goal_x, goal_y, None)
 
-    #
-    # TODO
-    # Implement the RRT algorithm for path planning
-    # The tree is already created with the corresponding starting node.
-    # Goal node is also already created.
-    # Return both, the tree and the path
-    #
-    
+    while goal_node.parent is None and max_attempts > 0:
+        [x,y] = get_random_q(grid_map)
+        nearest_node = get_nearest_node(tree, x, y)
+        new_node = get_new_node(nearest_node, x, y, epsilon)
+        if not check_collision(nearest_node, new_node, grid_map):
+            nearest_node.children.append(new_node)
+            if not check_collision(new_node, goal_node, grid_map):
+                new_node.children.append(goal_node)
+                goal_node.parent = new_node
+        max_attempts -= 1
         
     path = []
     while goal_node.parent is not None:
