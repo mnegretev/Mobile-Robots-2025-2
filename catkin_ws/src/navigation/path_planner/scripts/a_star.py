215a66fdcaff9b314cda7d42072dd44faa0461b9
Josearenas321 <112380408+Josearenas321@users.noreply.github.com>
2025-02-27 13:52:18 -0600

    math.sqrt

--- catkin_ws/src/navigation/path_planner/scripts/a_star.py
+++ catkin_ws/src/navigation/path_planner/scripts/a_star.py
@@ -51,7 +51,7 @@ def a_star(start_r, start_c, goal_r, goal_c, grid_map, cost_map, use_diagonals):
             if grid_map [r,c] > 40 or grid_map[r,c] < 0 or in_closed_list[r,c]:
                 continue
             g = g_values [row, col] + cost + cost_map [r][c]
-            h = math_sqrt((goal_r-r)**2 + (goal_c - c)**2)
+            h = math.sqrt((goal_r-r)**2 + (goal_c - c)**2)
             f = g + h
             if g < g_values[r,c]:
                 g_values[r,c] = g
