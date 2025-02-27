b9a6dbe6157d67c79cac31c21657e1f07a62d288
leogrief666 <leonardoramirosanchez616@gmail.com>
2025-02-27 03:55:48 -0600

    Update ros_basics.py

--- catkin_ws/src/navigation/simple_move/scripts/ros_basics.py
+++ catkin_ws/src/navigation/simple_move/scripts/ros_basics.py
@@ -7,12 +7,13 @@
 # Write a program to move the robot forwards until the laser
 # detects an obstacle in front of it.
 # Required publishers and subscribers are already declared and initialized.
+#
 
 import rospy
 from sensor_msgs.msg   import LaserScan
 from geometry_msgs.msg import Twist
 
-NAME = "Ramiro Sanchez Leonardo"
+NAME = "RAMIRO SANCHEZ LEONARDO"
     
 def callback_scan(msg):
     global obstacle_detected
@@ -21,8 +22,7 @@ def callback_scan(msg):
     # Do something to detect if there is an obstacle in front of the robot.
     # Set the 'obstacle_detected' variable with True or False, accordingly.
     #
-    
-    n = int((msg.angle_max - msg.angle_min)/msg.angle_increment/2)
+    n = int((msg.angle_max - msg.angle_min) / msg.angle_increment / 2)
     obstacle_detected = msg.ranges[n] < 1.0
     return
     
@@ -43,11 +43,16 @@ def main():
         # Move forward if there is no obstacle in front of the robot, and stop otherwise.
         # Use the 'obstacle_detected' variable to check if there is an obstacle. 
         # Publish the Twist message using the already declared publisher 'pub_cmd_vel'.
-        
         msg_cmd_vel = Twist()
         msg_cmd_vel.linear.x = 0 if obstacle_detected else 0.3
         pub_cmd_vel.publish(msg_cmd_vel)
         loop.sleep()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
 
 
 if __name__ == '__main__':
