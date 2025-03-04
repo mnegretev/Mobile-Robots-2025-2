4b2836a208bb887787a2366d7fcf7b8a3fb0b1e2
cruz_zamora <joeldavid.dev@gmail.com>
2025-03-03 20:45:49 -0600

    Tarea 6 correción error_a

--- catkin_ws/src/navigation/simple_move/scripts/path_follower.py
+++ catkin_ws/src/navigation/simple_move/scripts/path_follower.py
@@ -32,7 +32,7 @@ def calculate_control(robot_x, robot_y, robot_a, goal_x, goal_y, alpha, beta, v_
     #
     # TODO:
     # Implement the control law given by:
-    #
+    error_a = math.atan2(goal_y - robot_y, goal_x - robot_x) - robot_a
     v = v_max*math.exp(-error_a*error_a/alpha)
     w = w_max*(2/(1 + math.exp(-error_a/beta)) - 1)
     #
