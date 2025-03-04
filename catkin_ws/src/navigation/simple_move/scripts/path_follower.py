a829989c4eb17dcea295dced22ec32df602743fa
carloslujan196 <54092062+carloslujan196@users.noreply.github.com>
2025-03-04 10:52:05 -0600

    TAREA2 CAMBIOS EN LA VARIABLE DE CONTROL

--- catkin_ws/src/navigation/simple_move/scripts/path_follower.py
+++ catkin_ws/src/navigation/simple_move/scripts/path_follower.py
@@ -20,7 +20,7 @@ from nav_msgs.srv import GetPlan, GetPlanRequest
 from navig_msgs.srv import ProcessPath, ProcessPathRequest
 from geometry_msgs.msg import Twist, PoseStamped, Pose, Point
 
-NAME = "Lujan Perez Carlos Eduardo"
+NAME = "LUJAN PEREZ CARLOS EDUARDO"
 
 pub_goal_reached = None
 pub_cmd_vel = None
@@ -32,16 +32,16 @@ def calculate_control(robot_x, robot_y, robot_a, goal_x, goal_y, alpha, beta, v_
     #
     # TODO:
     # Implement the control law given by:
-    #
-    v = v_max*math.exp(-error_a*error_a/alpha)
-    w = w_max*(2/(1 + math.exp(-error_a/beta)) - 1)
+    e_a = math.atan2(goal_y-robot_y,goal_x-robot_x)-robot_a
+    e_a = (e_a+math.pi)%(2*math.pi)-math.pi
+    v = v_max*math.exp(-e_a*e_a/alpha)
+    w = w_max*(2/(1 + math.exp(-e_a/beta)) - 1)
     #
     # where error_a is the angle error
     # and v_max, w_max, alpha and beta, are tunning constants.
     # Remember to keep error angle in the interval (-pi,pi]
     # Return the tuple [v,w]
     #
-
     return [v,w]
 
 def follow_path(path, alpha, beta, v_max, w_max):
@@ -51,27 +51,26 @@ def follow_path(path, alpha, beta, v_max, w_max):
     # Path is given as a sequence of points [[x0,y0], [x1,y1], ..., [xn,yn]]
     # You can use the following steps to perform the path tracking:
     #
-    Set goal point as the first point of the path
-    Get robot position with Pr, robot_a = get_robot_pose()
+    # Set goal point as the first point of the path
+    # Get robot position with Pr, robot_a = get_robot_pose()
     #
-    WHILE distance to last point > tol and not rospy.is_shutdown() 
-        Calculate control signals v and w
-        Publish the control signals with the function publish_twist()
-        Get robot position
-        If dist to goal point is less than 0.3 (you can change this constant)
-            Change goal point to the next point in the path
+    # WHILE distance to last point > tol and not rospy.is_shutdown() 
+    #     Calculate control signals v and w
+    #     Publish the control signals with the function publish_twist()
+    #     Get robot position
+    #     If dist to goal point is less than 0.3 (you can change this constant)
+    #         Change goal point to the next point in the path
     #
-    id_x = 0
-    Pg = path[id_x]
-    Pr, robot_a = get_robot_pose()
-    while numpy.linalg.norm(path[-1] - Pr) > 0.1 and not rospy.is_shutdown():
-        v, w = calculate_control(Pr[0], Pr[1], robot_a, Pg[0], Pg[1], alpha, beta, v_max, w_max)
-        publish_twist(v, w)
-        Pr, robot_a = get_robot_pose()
-        if numpy.linalg.norm(Pg - Pr) < 0.3:
-            id_x = min(id_x + 1, len(path) - 1)
-            Pg = path[id_x]
-
+    idx = 0
+    pg = path[idx]
+    pr,robot_a = get_robot_pose()
+    while numpy.linalg.norm(path[-1]-pr)>0.1 and not rospy.is_shutdown():
+    	v,w = calculate_control(pr[0],pr[1],robot_a,pg[0],pg[1],alpha,beta,v_max,w_max)
+    	publish_and_save_data(pr[0], pr[1], robot_a, pg[0], pg[1],v,w)
+    	pr,robot_a=get_robot_pose()
+    	if numpy.linalg.norm(pg-pr)<0.3:
+    		idx=min(idx+1,len(path)-1)
+    		pg = path[idx]
     return
         
 
