710f2520c19a5c1bc11d3a99b029d455f925e989
leogrief666 <leonardoramirosanchez616@gmail.com>
2025-03-04 21:52:36 -0600

    Tarea 3

--- catkin_ws/src/navigation/map_augmenter/scripts/gvd.py
+++ catkin_ws/src/navigation/map_augmenter/scripts/gvd.py
@@ -51,9 +51,9 @@ def brushfire(grid_map):
             if distances[i+k1, j+k2] == -1:
                 Q.put([i+k1, j+k2])
                # distances[i+k1, j+k2] = d + 2
-                distances[i+k1, j+k2] = d + math.sqrt(2.0)
+                distances[i+k1, j+k2] = d + 2.0
             else:
-                distances[i+k1, j+k2] = min(distances[i+k1, j+k2], d+ 2)
+                distances[i+k1, j+k2] = min(distances[i+k1, j+k2], d+ math.sqrt(2.0))
                 #distances[i+k1, j+k2] = min(distances[i+k1, j+k2], d+ math.sqrt(2.0))
     return distances
 
