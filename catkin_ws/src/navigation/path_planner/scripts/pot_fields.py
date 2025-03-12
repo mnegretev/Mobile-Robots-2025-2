3850368d7572f9df39306f937c2fb589fa6fcba2
german182ro <german182romero@gmail.com>
2025-03-11 20:33:19 -0600

    tarea}

--- catkin_ws/src/navigation/path_planner/scripts/pot_fields.py
+++ catkin_ws/src/navigation/path_planner/scripts/pot_fields.py
@@ -24,7 +24,7 @@ laser_readings = None
 v_max = 0.6
 w_max = 1.0
 
-NAME = "FULL NAME"
+NAME = "German Zair Romero Hernandez"
 
 def calculate_control(goal_x, goal_y, alpha, beta):
     v,w = 0,0
@@ -47,6 +47,15 @@ def attraction_force(goal_x, goal_y, eta):
     # where force_x and force_y are the X and Y components
     # of the resulting attraction force w.r.t. robot
     #
+    q_g = numpy.asarray([goal_x, goal_y]) 
+    norm_q_g = numpy.linalg.norm(q_g) 
+
+    if norm_q_g != 0: 
+        unit_q_g = q_g / norm_q_g 
+        F_att = -eta * unit_q_g 
+        force_x, force_y = F_att[0], F_att[1] 
+    else:
+        force_x, force_y = 0, 0
     
     return numpy.asarray([force_x, force_y])
 
@@ -66,7 +75,18 @@ def rejection_force(laser_readings, zeta, d0):
     # where force_x and force_y are the X and Y components
     # of the resulting rejection force
     #
+    for reading in laser_readings:
+        distance, angle = reading 
+        if distance < d0:
+            rho = zeta * ( (1.0/distance) - (1.0/d0) ) * (1.0/(distance**2)) 
+        else:
+            rho = 0
 
+        force_x += rho * numpy.cos(angle) 
+        force_y += rho * numpy.sin(angle) 
+
+    force_x = force_x / N 
+    force_y = force_y / N 
     
     return numpy.asarray([force_x, force_y])
 
@@ -85,7 +105,22 @@ def move_by_pot_fields(global_goal_x, global_goal_y, epsilon, tol, eta, zeta, d0
     #    Calculate the control laws v,w to move the robots towards P
     #    Call the function publish_speed_and_forces(v, w, Fa, Fr, F) (moves the robot and displays forces)
     #    Get the goal point w.r.t. robot by calling the get_goal_point_wrt_robot function.
-    
+    goal_wrt_robot = get_goal_point_wrt_robot(global_goal_x, global_goal_y)
+    goal_x_r, goal_y_r = goal_wrt_robot[0], goal_wrt_robot[1]
+
+    while math.sqrt(goal_x_r**2 + goal_y_r**2) > tol and not rospy.is_shutdown():
+        Fa = attraction_force(goal_x_r, goal_y_r, eta)
+        Fr = rejection_force(laser_readings, zeta, d0)
+        F = Fa + Fr
+
+        goal_x_r = goal_x_r - epsilon * F[0] # Gradient descent update for goal_x_r
+        goal_y_r = goal_y_r - epsilon * F[1] # Gradient descent update for goal_y_r
+
+        [v, w] = calculate_control(goal_x_r, goal_y_r, alpha, beta)
+        publish_speed_and_forces(v, w, Fa, Fr, F)
+
+        goal_wrt_robot = get_goal_point_wrt_robot(global_goal_x, global_goal_y) # Re-evaluate goal wrt robot in global loop
+        goal_x_r, goal_y_r = goal_wrt_robot[0], goal_wrt_robot[1]
     return
         
 
