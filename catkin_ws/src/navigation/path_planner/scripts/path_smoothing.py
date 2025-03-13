ba27cef9123a393811e0b5b4835b6e46876bd101
Josearenas321 <112380408+Josearenas321@users.noreply.github.com>
2025-03-13 09:40:21 -0600

    nombre

--- catkin_ws/src/navigation/path_planner/scripts/path_smoothing.py
+++ catkin_ws/src/navigation/path_planner/scripts/path_smoothing.py
@@ -15,7 +15,7 @@ from geometry_msgs.msg import Pose, PoseStamped, Point
 from navig_msgs.srv import ProcessPath
 from navig_msgs.srv import ProcessPathResponse
 
-NAME = "FULL NAME"
+NAME = "JOSE AUGUSTO ARENAS HERNANDEZ"
 
 def smooth_path(Q, alpha, beta, max_steps):
     #
