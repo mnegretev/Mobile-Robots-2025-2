848c56f9ff1e3324c886fb3ce334091f7dfe9702
Joel David C. <jdcruzzamora@gmail.com>
2025-03-12 22:28:18 -0600

    Practica 2

--- catkin_ws/src/navigation/path_planner/scripts/path_smoothing.py
+++ catkin_ws/src/navigation/path_planner/scripts/path_smoothing.py
@@ -31,9 +31,16 @@ def smooth_path(Q, alpha, beta, max_steps):
     P = numpy.copy(Q)
     tol     = 0.00001                   
     nabla   = numpy.full(Q.shape, float("inf"))
-    epsilon = 0.1                       
+    epsilon = 0.1   
+    n = len(P)  
+    nabla[0] = 0
+    nabla[n-1] = 0                  
 
-    
+    while numpy.linalg.norm(nabla) > tol and steps < max_steps:
+        for i in range(1, n-1):
+            nabla[i] = alpha*(2*P[i] - P[i-1] - p[i+1]) + beta*(P[i] - Q[i])
+        P -= epsilon * nabla
+        steps += 1
     return P
 
 def callback_smooth_path(req):
