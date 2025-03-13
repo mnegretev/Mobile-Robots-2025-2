d9d1eee7a429c830788e5079027c6538903179b3
Josearenas321 <112380408+Josearenas321@users.noreply.github.com>
2025-03-13 05:05:17 -0600

    t6

--- catkin_ws/src/navigation/simple_move/scripts/path_follower.py
+++ catkin_ws/src/navigation/simple_move/scripts/path_follower.py
@@ -29,19 +29,13 @@ listener    = None
 
 def calculate_control(robot_x, robot_y, robot_a, goal_x, goal_y, alpha, beta, v_max, w_max):
     v,w = 0,0
-    #
-    # TODO:
-    # Implement the control law given by:
-    #
-    # v = v_max*math.exp(-error_a*error_a/alpha)
-    # w = w_max*(2/(1 + math.exp(-error_a/beta)) - 1)
-    #
-    # where error_a is the angle error
-    # and v_max, w_max, alpha and beta, are tunning constants.
-    # Remember to keep error angle in the interval (-pi,pi]
-    # Return the tuple [v,w]
-    #
 
+    error_a = math.atan2(goal_y - robot_y, goal_x - robot_x) - robot_a
+    error_a = (error_a + math.pi) % (2 * math.pi) - math.pi  # Keep error_a in (-pi, pi]
+
+    v = v_max * math.exp(-error_a * error_a / alpha)
+    w = w_max * (2 / (1 + math.exp(-error_a / beta)) - 1)
+    
     return [v,w]
 
 def follow_path(path, alpha, beta, v_max, w_max):
