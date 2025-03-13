4fcce4d05be69ae8f72b0df1dea7cd94666f4509
alan5412 <alanmisael54@gmail.com>
2025-03-13 11:42:26 -0600

    Practica2

--- catkin_ws/src/navigation/path_planner/scripts/path_smoothing.py
+++ catkin_ws/src/navigation/path_planner/scripts/path_smoothing.py
@@ -15,7 +15,7 @@ from geometry_msgs.msg import Pose, PoseStamped, Point
 from navig_msgs.srv import ProcessPath
 from navig_msgs.srv import ProcessPathResponse
 
-NAME = "FULL NAME"
+NAME = "Alan Misael Camarena Olivos"
 
 def smooth_path(Q, alpha, beta, max_steps):
     #
@@ -33,6 +33,13 @@ def smooth_path(Q, alpha, beta, max_steps):
     nabla   = numpy.full(Q.shape, float("inf"))
     epsilon = 0.1                       
 
+    nabla[0] = 0
+    nabla[-1] = 0
+    while numpy.linalg.norm(nabla) > tol and steps < max_steps:
+        for i in range(1,len(nabla)-1):
+            nabla[i] = alpha*(2*P[i] - P[i-1] - P[i+1]) + beta*(Q[i] - P[i])    
+        P = P - epsilon*nabla
+        steps += 1
     
     return P
 
