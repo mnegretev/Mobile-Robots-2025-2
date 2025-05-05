12b8cc7bb441ce5a54839a235e2ba878f057fda2
AlfredoGuadalupe <alfredoguadalupe.alcantara@gmail.com>
2025-05-05 13:16:25 -0600

    Práctica 4, con nombre completo

--- catkin_ws/src/vision/neural_network/scripts/nn_training.py
+++ catkin_ws/src/vision/neural_network/scripts/nn_training.py
@@ -14,7 +14,7 @@ import numpy
 import rospy
 import rospkg
 
-NAME = "FULL_NAME"
+NAME = "Alfredo Guadalupe Alcántara Guerrero"
 
 class NeuralNetwork(object):
     def __init__(self, layers, weights=None, biases=None):
