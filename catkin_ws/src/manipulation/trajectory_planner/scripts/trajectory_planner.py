b64884696c5e4209612645d167ba402d94993e39
Josearenas321 <112380408+Josearenas321@users.noreply.github.com>
2025-05-20 15:32:54 -0600

    Update trajectory_planner.py

--- catkin_ws/src/manipulation/trajectory_planner/scripts/trajectory_planner.py
+++ catkin_ws/src/manipulation/trajectory_planner/scripts/trajectory_planner.py
@@ -20,9 +20,12 @@ from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
 prompt = ""
 NAME = "JOSÉ AUGUSTO ARENAS HERNÁNDEZ"
-def get_polynomial_trajectory(q0, q1, dq0=0, dq1=0, ddq0=0, ddq1=1, t=1.0, step=0.05):
-    T = numpy.arange(0, t, step)
 
+
+def get_polynomial_trajectory(q0, q1, dq0=0, dq1=0, ddq0=0, ddq1=1, t=1.0, step=0.05):
+   
+    
+    T = numpy.arange(0, t, step)
     A = numpy.array([
         [t**5, t**4, t**3, t**2, t, 1],
         [5*t**4, 4*t**3, 3*t**2, 2*t, 1, 0],
@@ -31,13 +34,15 @@ def get_polynomial_trajectory(q0, q1, dq0=0, dq1=0, ddq0=0, ddq1=1, t=1.0, step=
         [0, 0, 0, 0, 1, 0],
         [0, 0, 0, 2, 0, 0]
     ])
-    # B = [q1, dq1, ddq1, q0, dq0, ddq0]
+
+
+    
     B = numpy.array([q1, dq1, ddq1, q0, dq0, ddq0])
-
     X = numpy.linalg.solve(A, B)
-    a5, a4, a3, a2, a1, a0 = X
-
+    a5, a4, a3, a2, a1, a0 = X  
     Q = a5*T**5 + a4*T**4 + a3*T**3 + a2*T**2 + a1*T + a0
+    
+    
     return T, Q
 
 
