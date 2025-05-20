516818fcaa715de717e16fd9a1401772342eb3eb
Josearenas321 <112380408+Josearenas321@users.noreply.github.com>
2025-05-19 18:13:59 -0600

    Update ik_numeric.py

--- catkin_ws/src/manipulation/inverse_kinematics/scripts/ik_numeric.py
+++ catkin_ws/src/manipulation/inverse_kinematics/scripts/ik_numeric.py
@@ -46,12 +46,19 @@ def forward_kinematics(q, T, W):
     #     http://docs.ros.org/en/jade/api/tf/html/python/transformations.html
     #
     H = tft.identity_matrix()
-    for i in range(len(q)):
-    	H = tft.concatenate_matrices(H,T[i],tft.rotation_matrix(q[i], W[i]))
-    H = tft.concatenate_matrices(H,T[7])
-    x,y,z = H[0,3],H[1,3],H[2,3]
-    R,P,Y = list(tft.euler_from_matrix(H))
+    for i in range(7):
+        Ri = tft.rotation_matrix(q[i], W[i])
+        H = tft.concatenate_matrices(H, T[i], Ri)
+    H = tft.concatenate_matrices(H, T[7])
+
+    trans = tft.translation_from_matrix(H)
+    rot = tft.euler_from_matrix(H, axes='sxyz')
+
+    x, y, z = trans
+    R, P, Y = rot
+
     return numpy.asarray([x,y,z,R,P,Y])
+
 
 def jacobian(q, T, W):
     delta_q = 0.000000001
