d3ac77018e95fb90e94b6e73c8cddac29aa5e942
carloslujan196 <54092062+carloslujan196@users.noreply.github.com>
2025-05-27 00:24:29 -0600

    Create speech_mic_node.py

---
+++ catkin_ws/src/planning/task_planner/scripts/speech_mic_node.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import rospy
+import speech_recognition as sr
+from hri_msgs.msg import RecognizedSpeech
+
+def main():
+    rospy.init_node('speech_mic_node')
+    pub = rospy.Publisher('/hri/sp_rec/recognized', RecognizedSpeech, queue_size=1)
+    recognizer = sr.Recognizer()
+    mic = sr.Microphone()
+    rospy.loginfo("Micrófono listo. Habla una orden...")
+
+    with mic as source:
+        recognizer.adjust_for_ambient_noise(source)
+
+    while not rospy.is_shutdown():
+        with mic as source:
+            rospy.loginfo("Escuchando...")
+            audio = recognizer.listen(source)
+
+        try:
+            sentence = recognizer.recognize_google(audio).upper()
+            rospy.loginfo("Reconocido: " + sentence)
+            msg = RecognizedSpeech()
+            msg.hypothesis = [sentence]
+            pub.publish(msg)
+        except sr.UnknownValueError:
+            rospy.logwarn("No entendí lo que dijiste.")
+        except sr.RequestError as e:
+            rospy.logerr("Error al acceder al servicio de reconocimiento: {0}".format(e))
+
+if __name__ == '__main__':
+    main()
