79afebe204db4a849e5ae648c9d7164d85e1dd61
carloslujan196 <54092062+carloslujan196@users.noreply.github.com>
2025-05-28 23:13:12 -0600

    Update final_project.py

--- catkin_ws/src/planning/task_planner/scripts/final_project.py
+++ catkin_ws/src/planning/task_planner/scripts/final_project.py
@@ -383,12 +383,12 @@ def main():
 
                 current_state = "SM_Prepare"
 
-        except Exception as e:
-            print("Error while trying to find object:", e)
-            say("I couldn't find the object.")
-            executing_task = False
-            new_task = False
-            current_state = "SM_Waiting"
+              except Exception as e:
+                  print("Error while trying to find object:", e)
+                   say("I couldn't find the object.")
+                    executing_task = False
+                    new_task = False
+                    current_state = "SM_Waiting"
 
             
             #Encontrar el objeto a buscar en la mesa estamos aqui
